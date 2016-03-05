99b3fa466401b61833d9f3cea4d754736d25eae5
Anthony Sneed <tony@tonysneed.com>
2016-03-05 12:37:46 +0100

    Added Gulp tasks for injecting scripts and imports.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-var gulp = require('gulp');
-var exec = require('child_process').exec;
-var browserSync = require('browser-sync');
+var gulp = require('gulp'),
+    glob = require('glob'),
+    path = require('path'),
+    exec = require('child_process').exec,
+    browserSync = require('browser-sync');
+var $ = require('gulp-load-plugins')({ lazy: true });
+
+gulp.task('help', $.taskListing.withFilters(/:/));
+gulp.task('default', ['help']);
 
 gulp.task('compile', function () {
     
@@ -12,7 +18,7 @@ gulp.task('watch', ['compile'], function () {
     return gulp.watch('./src/**/*.ts', ['compile']);
 });
 
-gulp.task('test', ['watch'], function () {
+gulp.task('test', ['specs:inject', 'imports:inject', 'watch'], function () {
     
     var options = {
         port: 3000,
@@ -30,3 +36,55 @@ gulp.task('test', ['watch'], function () {
     
     browserSync(options);
 });
+
+gulp.task('specs:inject', function () {
+
+    var source = [
+        './dist/**/*.js',
+        '!./dist/**/*.js.map',
+        '!./dist/**/*.spec.js'];
+        
+    var specs = ['./dist/**/*.spec.js'];
+
+    return gulp
+        .src('./specrunner.html')
+        .pipe(injectScripts(source, ''))
+        .pipe(injectScripts(specs, 'specs'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('imports:inject', function(){
+    
+    gulp.src('./util/system.template.js')
+        .pipe(injectImports(['./dist/**/*.spec.js'], 'import'))
+        .pipe($.rename('./util/system.imports.js'))
+        .pipe(gulp.dest('./'));
+});
+
+function injectScripts(src, label) {
+    
+    var options = { read: false, addRootSlash: false };
+    if (label) {
+        options.name = 'inject:' + label;
+    }
+    return $.inject(gulp.src(src), options);
+}
+
+function injectImports(src, label) {
+    
+    var search = '/// inject:' + label;
+    var first = '\n    System.import(\'';
+    var last = '\')';
+    var specNames = [];
+
+    src.forEach(function(pattern) {
+        glob.sync(pattern)
+            .forEach(function(file) {
+                var fileName = path.basename(file, path.extname(file));
+                var specName = path.join(path.dirname(file), fileName);
+                specNames.push(first + specName + last);
+            });
+    });
+
+    return $.injectString.after(search, specNames);
+}
---
+++ util/system.imports.js
@@ -0,0 +1,14 @@
+/* global System */
+
+// Configure systemjs to use the .js extension for imports from the src/js folder
+System.config({
+    packages: {
+        'dist': {defaultExtension: 'js'}
+    }
+});
+
+Promise.all([
+    /// inject:import
+    System.import('dist/greeter/greeter.spec'),
+    System.import('dist/italiangreeter/italiangreeter.spec')
+]);
